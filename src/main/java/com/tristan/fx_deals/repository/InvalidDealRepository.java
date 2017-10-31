9ea989f6522c9c011f44eef4ce38d74576cde82f
Tristan Diaz <tristan.diaz@voyagerinnovation.com>
2017-11-01 01:46:31 +0800

    implement jparepository instead of crudrepository

--- src/main/java/com/tristan/fx_deals/repository/InvalidDealRepository.java
+++ src/main/java/com/tristan/fx_deals/repository/InvalidDealRepository.java
@@ -1,7 +1,7 @@
 package com.tristan.fx_deals.repository;
 
 import com.tristan.fx_deals.domain.InvalidDeal;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
@@ -10,6 +10,6 @@ import java.util.UUID;
  * Created by tristandiaz on 10/29/17.
  */
 @Repository
-public interface InvalidDealRepository extends CrudRepository<InvalidDeal, UUID> {
+public interface InvalidDealRepository extends JpaRepository<InvalidDeal, UUID> {
 
 }
--- src/main/java/com/tristan/fx_deals/repository/ValidDealRepository.java
+++ src/main/java/com/tristan/fx_deals/repository/ValidDealRepository.java
@@ -1,8 +1,7 @@
 package com.tristan.fx_deals.repository;
 
 import com.tristan.fx_deals.domain.ValidDeal;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -12,8 +11,7 @@ import java.util.UUID;
  * Created by tristandiaz on 10/29/17.
  */
 @Repository
-public interface ValidDealRepository extends CrudRepository<ValidDeal, UUID> {
+public interface ValidDealRepository extends JpaRepository<ValidDeal, UUID> {
 
-    @Query("SELECT validDeal FROM ValidDeal validDeal WHERE validDeal.dealId = :dealId")
     ValidDeal findByDealId(@Param("dealId") String dealId);
 }
